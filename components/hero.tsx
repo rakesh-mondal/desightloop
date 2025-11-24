53bc0d595c9e05b7c5356548b04e02d6a09f86c3
Rakesh Mondal <hello@rakeshmondal.in>
2025-11-24 10:55:32 +0530

    Update hero section video to use local file sfx 2.1_updated.mp4 (video file excluded from git due to size)

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,7 +6,7 @@ export function Hero() {
   return (
     <ScrollExpandMedia
       mediaType="video"
-      mediaSrc="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/output_progressive_57c1b9d3-b52e-458a-b0b2-b69c00f2bcab-prptCsO61MNKzhkWaPWEVi5i6gL5RN.mp4"
+      mediaSrc="/sfx 2.1_updated.mp4"
       bgImageSrc="/background-image.jpg"
       title="Empowering Corporate Gifting"
       scrollToExpand="Scroll to Explore"
